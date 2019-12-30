8ff07d39f9aafce4036f6922ff505a52b8d703f1
Joshua Pratt <jp10010101010000@gmail.com>
2019-12-30 16:40:49 +1100

    With to node trait

--- src/ast.rs
+++ src/ast.rs
@@ -11,16 +11,40 @@ pub struct Apply {
 #[derive(Clone)]
 pub struct Sym {
     pub name: String,
+    pub info: Info,
+}
+
+impl ToNode Sym {
+    to_node(self) {
+        SymNode(self)
+    }
+    get_info(self) {
+        self.info
+    }
 }
 
 #[derive(Debug)]
 #[derive(PartialEq)]
 #[derive(Clone)]
 pub enum Prim {
-    Unit,
-    Bool(bool),
-    I32(i32),
-    Str(String),
+    Unit(Info),
+    Bool(bool, Info),
+    I32(i32, Info),
+    Str(String, Info),
+}
+
+impl ToNode Prim {
+    to_node(self) {
+        PrimNode(self)
+    }
+    get_info(self) {
+        match self {
+            Unit(info) => info,
+            Bool(_, info) => info,
+            I32(_, info) => info,
+            Str(_, info) => info,
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -29,6 +53,16 @@ pub enum Prim {
 pub struct Let {
     pub name: String,
     pub value: Option<Box<Node>>,
+    pub info: Info,
+}
+
+impl ToNode Let {
+    to_node(self) {
+        LetNode(self)
+    }
+    get_info(self) {
+        self.info
+    }
 }
 
 #[derive(Debug)]
@@ -37,6 +71,16 @@ pub struct Let {
 pub struct UnOp {
     pub name: String,
     pub inner: Box<Node>,
+    pub info: Info,
+}
+
+impl ToNode UnOp {
+    to_node(self) {
+        UnOpNode(self)
+    }
+    get_info(self) {
+        self.info
+    }
 }
 
 #[derive(Debug)]
@@ -46,6 +90,38 @@ pub struct BinOp {
     pub name: String,
     pub left: Box<Node>,
     pub right: Box<Node>,
+    pub info: Info,
+}
+
+impl ToNode BinOp {
+    to_node(self) {
+        BinOpNode(self)
+    }
+    get_info(self) {
+        self.info
+    }
+}
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+#[derive(Clone)]
+pub struct Loc {
+    filename: Option<String>;
+    line: i32;
+    col: i32;
+}
+
+#[derive(Debug)]
+#[derive(PartialEq)]
+#[derive(Clone)]
+pub struct Info {
+    loc: Option<Loc>;
+}
+
+impl Default Info {
+    fn default() -> Info {
+        Info {loc: None}
+    }
 }
 
 #[derive(Debug)]
@@ -59,6 +135,15 @@ pub enum Node {
     LetNode(Let),
     UnOpNode(UnOp),
     BinOpNode(BinOp),
+}
+
+pub trait ToNode {
+    fn to_node(self: Self) -> Node;
+    fn get_info(self: Self) -> Info;
+}
+
+pub fn get_loc<T: ToNode>(n: T) -> Option<Loc> {
+    n.get_info().loc
 }
 
 pub trait Visitor<State, Res, Final, Err> {
